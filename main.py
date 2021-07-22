801fe91ff3bf4b7eea51874c9bcb73a97f525baf
carlschr <cjcarlson136@gmail.com>
2021-07-22 14:28:55 -0500

    Add func for printing puzzle + func for inputs

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import random
 import copy
+import sys
+
+# helper function to check if a string can be converted to int
+def valid_integer(string):
+    try:
+        int(string)
+        return True
+    except ValueError:
+        return False
+
+
+# locates the nearest empty sudoku space
+def find_empty_space(puzzle):
+    for i in range(0, 9):
+        for j in range(0, 9):
+            if puzzle[i][j] == 0:
+                return [i, j]
+    return False
 
 def check_valid_num(puzzle, num, coord):
     # determine starting points for the sudoku square
@@ -24,14 +42,6 @@ def check_valid_num(puzzle, num, coord):
 def check_sudoku(puzzle_object):
     # copy of object to prevent mutations
     object_copy = copy.deepcopy(puzzle_object)
-
-    # locates the nearest empty sudoku space
-    def find_empty_space(puzzle):
-        for i in range(0, 9):
-            for j in range(0, 9):
-                if puzzle[i][j] == 0:
-                    return [i, j]
-        return False
 
     # solves a sudoku and returns a boolean for its solvability
     def solve_sudoku(coord):
@@ -141,12 +151,84 @@ def generate_sudoku():
 
     return puzzle_obj
 
-  
+# function to print a nice looking sudoku puzzle
+def print_sudoku(sudoku):
+    row_strings = []
+    for row in sudoku:
+        # each row will start with a pipe
+        row_string = '|'
 
+        for i, num in enumerate(row):
+            # the pipe for col 9 comes later
+            if i == 8:
+                row_string += str(num)
+            # each col that isn't 3, 6, 9 will be followed by a space
+            elif (i + 1) % 3 != 0:
+                row_string += str(num) + ' '
+            # col 3 and 6 are followed by a pipe
+            else:
+                row_string += str(num) + '|'
 
-new_sudoku = generate_sudoku()
-for row in new_sudoku['solved_puzzle']:
-    print(row)
-print('\n')
-for row in new_sudoku['puzzle']:
-    print(row)
+        row_strings.append(row_string)
+    
+    # top line
+    print(' _________________')
+
+    # rows are printed with a pipe on the end
+    for i, row_string in enumerate(row_strings):
+        # rows 3, 6 and 9 are underlined
+        if not (i + 1) % 3:
+            print("\u0332".join(row_string) + '|')
+        else:
+            print(f'{row_string}|')
+
+# function to start the application
+def play():
+    new_sudoku = generate_sudoku()
+    playing = True
+
+    while playing:
+        print_sudoku(new_sudoku['puzzle'])
+
+        # gathers input from user
+        arg = input('Enter an answer for a tile. (e.g. "r3c2 4"): ')
+        # arg must be at least 6 chars and char 2, 4, and 6 must be valid integers
+        while len(arg) < 6 or not valid_integer(arg[1]) or not valid_integer(arg[3]) or not valid_integer(arg[5]):
+            arg = input('Enter an answer for a tile. (e.g. "r3c2 4"): ')
+
+        row = int(arg[1])
+        col = int(arg[3])
+        num = int(arg[5])
+
+        # if the arg is correct, add it to the working puzzle
+        if new_sudoku['solved_puzzle'][row - 1][col - 1] == num:
+            new_sudoku['puzzle'][row - 1][col - 1] = num
+
+            # if the puzzle is filled ask if they want to keep playing
+            if not find_empty_space(new_sudoku['puzzle']):
+                print('Congrats, you finished the puzzle!')
+                keep_playing = input('Keep playing? Y/N: ')
+
+                # switch statement function
+                def switch(argument):
+                    switch_options = {
+                        'Y': True,
+                        'N': False
+                    }
+                    return switch_options.get(argument, False)
+
+                response = switch(keep_playing.upper())
+                if response:
+                    play()
+                else:
+                    playing = False
+
+            print('Got it, keep going!')
+        else:
+            print('Oops, That wasn\'t right!')
+
+    # kills program if playing is set to False
+    sys.exit()
+
+# program start
+play()
