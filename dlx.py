2f66e6ca91451130797ef0bfee81e31223e18f54
carlschr <cjcarlson136@gmail.com>
2021-08-05 18:44:12 -0500

    Converted cover matrix into quadlinked list

--- dlx.py
+++ dlx.py
@@ -1,10 +1,10 @@
 class Node:
-    def __init__(self, value=None):
+    def __init__(self, value=None, coord=None):
         self.up = None
         self.down = None
         self.left = None
         self.right = None
-        self.coord = [None, None]
+        self.coord = coord
         self.value = value
     
     def set_right(self, node):
@@ -35,78 +35,47 @@ class Node:
     def get_col(self):
         return self.coord[1]
 
-class CircularDoublyLinkedList:
-    def __init__(self, direction):
-        self.central_node = None
+class TwoDimensionalLinkedList:
+    def __init__(self):
+        self.head = None
         self.tail = None
         self.size = 0
-        self.direction = direction
+
     
-    def get_central_node(self):
-        return self.central_node
-    def get_tail(self):
-        return self.tail
 
-    def set_central_node(self, node):
-        self.central_node = node
-    def set_tail(self, node):
-        self.tail = node
-
-    def add_node(self, value):
-        node = Node(value)
-        if self.size == 0:
-            self.set_central_node(node)
-            self.set_tail(node)
-            self.size = 1
-            return node
-
-        central_node = self.get_central_node()
-        tail = self.get_tail()
-
-        if self.direction:
-            tail.set_down(node)
-            node.set_down(central_node)
-            node.set_up(tail)
-            central_node.set_up(node)
-            node.set_col(central_node.get_col())
-        else:
-            tail.set_right(node)
-            node.set_right(central_node)
-            node.set_left(tail)
-            central_node.set_left(node)
-            node.set_row(central_node.get_row())
-
-        self.set_tail(node)
-        self.size += 1
-        return node
+def create_rows_cols(head_node):
+    # creates columns
+    current_node = head_node
+    for i in range(81*4):
+        temp = Node(i)
+        current_node.set_right(temp)
+        temp.set_left(current_node)
+        current_node = temp
+        if i == 323:
+            temp.set_right(head_node)
+            head_node.set_left(temp)
     
-    def __repr__(self) -> str:
-        string = ''
-        central_node = self.get_central_node()
+    # creates rows
+    current_node = head_node
+    for i in range(81*9):
+        temp = Node(i)
+        current_node.set_down(temp)
+        temp.set_up(current_node)
+        current_node = temp
+        if i == 728:
+            temp.set_down(head_node)
+            head_node.set_up(temp)
 
-        if central_node:
-            current_node = central_node.get_down() if self.direction else central_node.get_right()
-            if current_node:
-                while current_node is not central_node:
-                    string += str(current_node.value)
-                    string += '\n'
-                    current_node = current_node.get_down() if self.direction else current_node.get_right()
+def index_to_coord(i, row_len):
+    row = (i - (i % row_len))/row_len
+    col = i % row_len
+    return [row, col]
 
-        return string
-
-h = CircularDoublyLinkedList(0)
-h.add_node('h')
-for i in range(81 * 4):
-    h.add_node(i)
-
-v = CircularDoublyLinkedList(1)
-v.add_node('v')
-for j in range(81 * 9):
-    v.add_node(j)
-
-print(v)
+def coord_to_index(row, col, row_len):
+    return int(row * row_len + col)
 
 
+            
 class Sudoku:
     def __init__(self, grid):
         self.grid = [[0 for j in range(9)] for i in range(9)]
@@ -116,7 +85,8 @@ class Sudoku:
 
 class Cover:
     def __init__(self):
-        self.matrix = [[0 for j in range(81 * 4)] for i in range(9 * 81)]
+        self.matrix = [[Node(0, [i, j]) for j in range(81 * 4)] for i in range(9 * 81)]
+        self.head = self.matrix[0][0]
         for i, row in enumerate(self.matrix):
             cell_number = int((i - (i % 9))/9)
             row_number = int((i - (i % 81))/81)
@@ -124,41 +94,53 @@ class Cover:
             box_number = int((row_number - (row_number % 3)) + ((col_number - (col_number % 3))/3))
 
             #cell
-            row[cell_number] = 1
+            cell_i = cell_number
+            row[cell_i].value = 1
+            self.connect_one_above(i, cell_i)
+            self.connect_one_left(i, cell_i)
+
             #row
-            row[((row_number * 9) + (i % 9)) + 81] = 1
+            row_i = ((row_number * 9) + (i % 9)) + 81
+            row[row_i].value = 1
+            self.connect_one_above(i, row_i)
+            self.connect_one_left(i, row_i)
+
             #col
-            row[(i % 81) + 81 * 2] = 1
+            col_i = (i % 81) + 81 * 2
+            row[col_i].value = 1
+            self.connect_one_above(i, col_i)
+            self.connect_one_left(i, col_i)
+
             #box
-            row[((box_number * 9) + (i % 9)) + 81 * 3] = 1
+            box_i = ((box_number * 9) + (i % 9)) + 81 * 3
+            row[box_i].value = 1
+            self.connect_one_above(i, box_i)
+            self.connect_one_left(i, box_i)
+
+    def connect_one_above(self, i, j):
+        n = i - 1
+        while n > 0:
+            if self.matrix[n][j].value:
+                current = self.matrix[i][j]
+                found = self.matrix[n][j]
+                current.up = found
+                found.down = current
+                return found.value
+            n += 1
+        return 0
+
+    def connect_one_left(self, i, j):
+        n = j - 1
+        while n > 0:
+            if self.matrix[i][n].value:
+                current = self.matrix[i][j]
+                found = self.matrix[i][n]
+                current.left = found
+                found.right = current
+                return found.value
+            n += 1
+        return 0
 
 cover = Cover()
 
-# for i, row in enumerate(cover.matrix):
-
-#     if i < 81:
-#         print(row[:81])
-
-# input()
-# print('\n\n\n')
-
-# for i, row in enumerate(cover.matrix):
-
-#     if i < 81:
-#         print(row[81:162])
-
-# input()
-# print('\n\n\n')
-
-# for i, row in enumerate(cover.matrix):
-
-#     if i < 81:
-#         print(row[162:243])
-
-# input()
-# print('\n\n\n')
-
-# for i, row in enumerate(cover.matrix):
-
-#     if i < 81:
-#         print(row[243:324])        
+my_arr = cover.matrix
